a7b6158aa1284827320956b89a522aed8cb8a430
Jpta24 <jeanpierretorres@gmail.com>
2022-11-14 07:49:56 +0100

    clean routes to update user

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -38,83 +38,188 @@ router.put('/:userID', (req, res, next) => {
     const userID = req.params.userID
     const {update} = req.body
 
-    if (update === 'rol') {
-        const {rol, buzname} = req.body
-        // console.log(rol);
+    // if (update === 'rol') {
+    //     const {rol, buzname} = req.body
+    //     // console.log(rol);
     
-        Business.findOne({ name:buzname})
-            .then((foundBusiness) => {
-                if (!foundBusiness) {
-                    res.status(400).json({ message: 'Business does not exists.' });
-                return;
-                }
-                return User.findByIdAndUpdate(userID,{business:foundBusiness._id,rol:rol},{new:true})
-                .then((user)=>{
-                    return Business.findByIdAndUpdate(foundBusiness._id,{$push:{'employees':user._id}},{new:true})
-                }).then(businessUpdated =>{
-                    res.status(200).json({ business:businessUpdated });
-                })
-                .catch(err => {
-                    console.log(err)
-                    res.status(500).json({ message: "Sorry internal error occurred" })
-                  });
+    //     Business.findOne({ name:buzname})
+    //         .then((foundBusiness) => {
+    //             if (!foundBusiness) {
+    //                 res.status(400).json({ message: 'Business does not exists.' });
+    //             return;
+    //             }
+    //             return User.findByIdAndUpdate(userID,{business:foundBusiness._id,rol:rol},{new:true})
+    //             .then((user)=>{
+    //                 return Business.findByIdAndUpdate(foundBusiness._id,{$push:{'employees':user._id}},{new:true})
+    //             }).then(businessUpdated =>{
+    //                 res.status(200).json({ business:businessUpdated });
+    //             })
+    //             .catch(err => {
+    //                 console.log(err)
+    //                 res.status(500).json({ message: "Sorry internal error occurred" })
+    //               });
     
-                // return Business.findByIdAndUpdate(foundBusiness._id,{employee})
-            }).catch(err => {
-                console.log(err)
-                res.status(500).json({ message: "Sorry internal error occurred" })
-              });
-    }
+    //             // return Business.findByIdAndUpdate(foundBusiness._id,{employee})
+    //         }).catch(err => {
+    //             console.log(err)
+    //             res.status(500).json({ message: "Sorry internal error occurred" })
+    //           });
+    // }
 
-    if (update === 'cart') {
+    // if (update === 'cart') {
     
-        // console.log(update)
-        const {cart} = req.body
-        User.findByIdAndUpdate(userID,{$push:{cart:cart}})
-        .then((user)=>{
-            console.log(user)
-            res.status(200).json(user)})
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: "Sorry internal error occurred" })
-          });
-    }
+    //     // console.log(update)
+    //     const {cart} = req.body
+    //     User.findByIdAndUpdate(userID,{$push:{cart:cart}})
+    //     .then((user)=>{
+    //         console.log(user)
+    //         res.status(200).json(user)})
+    //     .catch(err => {
+    //         console.log(err)
+    //         res.status(500).json({ message: "Sorry internal error occurred" })
+    //       });
+    // }
     
-    if (update === 'removeCart'){
-        const {cart} = req.body
-        User.findByIdAndUpdate(userID,{$pull: { cart: { _id: cart.product }}})
+    // if (update === 'removeCart'){
+    //     const {cart} = req.body
+    //     User.findByIdAndUpdate(userID,{$pull: { cart: { _id: cart.product }}})
+    //         .then((user)=>{
+    //             res.status(200).json(user)})
+    //         .catch(err => {
+    //             console.log(err)
+    //             res.status(500).json({ message: "Sorry internal error occurred" })
+    //           });
+    // }
+
+    // if(update==='order'){
+    //     const {orders} = req.body;
+    //     orders.map(order=>{
+    //         let orderID 
+    //         Order.create(order)
+    //         .then(createdOrder=>{
+    //             console.log({createdOrder});
+    //             orderID = createdOrder._id
+    //             return User.findByIdAndUpdate(order.user,{$push:{orders:createdOrder._id}, $set: { cart: [] }})
+    //         }).then(userUpdated=>{
+    //             console.log(orderID);
+    //             return Business.findByIdAndUpdate(order.business,{$push:{orders:orderID}})
+    //         }).then((updatedBuz)=>{
+    //             res.status(200).json(updatedBuz)
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //             res.status(500).json({ message: "Sorry internal error occurred" })
+    //           });
+
+    //     })
+    // }
+
+   	
+});
+
+router.put('/rol/:userID', (req, res, next) => {
+    const userID = req.params.userID
+
+    const {rol, buzname} = req.body
+
+    Business.findOne({ name:buzname})
+        .then((foundBusiness) => {
+            if (!foundBusiness) {
+                res.status(400).json({ message: 'Business does not exists.' });
+            return;
+            }
+            return User.findByIdAndUpdate(userID,{business:foundBusiness._id,rol:rol},{new:true})
             .then((user)=>{
-                res.status(200).json(user)})
-            .catch(err => {
-                console.log(err)
-                res.status(500).json({ message: "Sorry internal error occurred" })
-              });
-    }
-
-    if(update==='order'){
-        const {orders} = req.body;
-        orders.map(order=>{
-            let orderID 
-            Order.create(order)
-            .then(createdOrder=>{
-                console.log({createdOrder});
-                orderID = createdOrder._id
-                return User.findByIdAndUpdate(order.user,{$push:{orders:createdOrder._id}, $set: { cart: [] }})
-            }).then(userUpdated=>{
-                console.log(orderID);
-                return Business.findByIdAndUpdate(order.business,{$push:{orders:orderID}})
-            }).then((updatedBuz)=>{
-                res.status(200).json(updatedBuz)
+                return Business.findByIdAndUpdate(foundBusiness._id,{$push:{'employees':user._id}},{new:true})
+            }).then(businessUpdated =>{
+                res.status(200).json({ business:businessUpdated });
             })
             .catch(err => {
                 console.log(err)
                 res.status(500).json({ message: "Sorry internal error occurred" })
-              });
+                });
 
+            // return Business.findByIdAndUpdate(foundBusiness._id,{employee})
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Sorry internal error occurred" })
+            });
+    
+});
+
+router.put('/addCart/:userID', (req, res, next) => {
+    const userID = req.params.userID
+
+    const {cart} = req.body
+    User.findByIdAndUpdate(userID,{$push:{cart:cart}})
+    .then((user)=>{
+        console.log(user)
+        res.status(200).json(user)})
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "Sorry internal error occurred" })
+        });
+    
+});
+
+router.put('/removeCart/:userID', (req, res, next) => {
+    const userID = req.params.userID
+    
+    const {product} = req.body
+    User.findByIdAndUpdate(userID,{$pull: { cart: { _id: product }}})
+        .then((user)=>{
+            res.status(200).json(user)})
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Sorry internal error occurred" })
+            });
+});
+
+router.put('/order/:userID', (req, res, next) => {
+    const userID = req.params.userID
+    
+    const {orders} = req.body;
+    orders.map(order=>{
+        let orderID 
+        let userUpdated
+        Order.create(order)
+        .then(createdOrder=>{
+            orderID = createdOrder._id
+            return User.findByIdAndUpdate(order.user,{$push:{orders:createdOrder._id}, $set: { cart: [] }},{new:true}).populate('business')
+            .populate(({
+                path: 'cart',
+                populate: {
+                  path: "product",
+                    populate: {
+                        path:"business"
+                    }
+                }
+              })).populate(({
+                path: 'orders',
+                populate: {
+                  path: "business"
+                }
+              })).populate(({
+                path: 'orders',
+                populate: {
+                  path: "products",
+                    populate: {
+                        path: "product"
+                    }
+                }
+              }))
+        }).then(user=>{
+            userUpdated = user
+            return Business.findByIdAndUpdate(order.business,{$push:{orders:orderID}})
+        }).then(()=>{
+            res.status(200).json(userUpdated)
         })
-    }
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Sorry internal error occurred" })
+            });
 
-   	
+    })
 });
 
 module.exports = router;
